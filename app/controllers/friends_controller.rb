3dcc80a1a7f07c570d4ac5e0b4fe96b470f8675b
Michael Bashour <michael.s.bashour@gmail.com>
2015-02-05 15:18:00 -0600

    Basic start

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,26 @@
+class FriendsController < ApplicationController
+	def create
+		@friend = Friend.new(friend_params)
+		@friend.save
+		redirect_to @friend
+	end
+
+	def new
+	end
+
+	def index
+		@friends = Friend.all
+
+	end
+	
+	def show
+		@friend = Friend.find(params[:id])
+		@tweets = CLIENT.user_timeline(@friend.twitterHandle)
+	end
+	
+	private
+		def friend_params
+			params.require(:friend).permit(:firstName, :twitterHandle)
+		end
+
+end
---
+++ config/initializers/twitter_initializers.rb
@@ -0,0 +1,5 @@
+require 'twitter'
+CLIENT = Twitter::REST::Client.new do |config|
+	config.consumer_key = "JICkrmsaDOFke5SGwqNii5IOw"
+	config.consumer_secret = "H8Ts2xjrSGdOkXqbIUx386RTXk0soRuTE4c7YBwGJ1Yd7gYeYs"
+end
