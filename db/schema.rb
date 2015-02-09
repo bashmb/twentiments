6bbebab2b187d6fc6214c8cf8f1576f5629415ec
Michael Bashour <michael.s.bashour@gmail.com>
2015-02-08 21:30:19 -0600

    Alter db for use with PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150205055842) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friends", force: :cascade do |t|
     t.string   "firstName"
     t.string   "twitterHandle"
