d6e3a386c5c147f6ee04b1f0f74b2c182f7b826e
Michael Bashour <michael.s.bashour@gmail.com>
2015-02-12 14:14:24 -0600

    Calculate score in Tweets table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212173916) do
+ActiveRecord::Schema.define(version: 20150212200748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150212173916) do
     t.integer  "tweetTime"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "tweetScore"
   end
 
 end
--- lib/tasks/getTweets.rb
+++ lib/tasks/getTweets.rb
@@ -1,15 +1,14 @@
-require "twitter"
 # get most recent 10 tweets
-friend = "dalailama"
+friend = "mindeavor"
 tweets = CLIENT.user_timeline(friend)
 (10).downto(0).each do |i|
 	newTweet = Tweet.new
-	newTweet.twitterHandle = friend
-	newTweet.tweetText = tweets[i].text
 	newTweet.tweetTime = tweets[i].created_at.to_i
 	lastTweetTime = Tweet.where(twitterHandle:friend).maximum("tweetTime")
 	if lastTweetTime == nil or newTweet.tweetTime > lastTweetTime
+		newTweet.twitterHandle = friend
+		newTweet.tweetText = tweets[i].text.chomp
+		newTweet.tweetScore = (Indico.sentiment(newTweet.tweetText)*100).round
 		newTweet.save
 	end
-
 end
