d8681e35740446e7c7f5b82c8da101336aa474f7
Michael Bashour <michael.s.bashour@gmail.com>
2015-02-12 15:56:54 -0600

    Run getTweets for all friends

--- lib/tasks/getTweets.rb
+++ lib/tasks/getTweets.rb
@@ -1,14 +1,16 @@
 # get most recent 10 tweets
-friend = "mikebashour"
-tweets = CLIENT.user_timeline(friend)
-(10).downto(0).each do |i|
-	newTweet = Tweet.new
-	newTweet.tweetTime = tweets[i].created_at.to_i
-	lastTweetTime = Tweet.where(twitterHandle:friend).maximum("tweetTime")
-	if lastTweetTime == nil or newTweet.tweetTime > lastTweetTime
-		newTweet.twitterHandle = friend
-		newTweet.tweetText = tweets[i].text.chomp
-		newTweet.tweetScore = (Indico.sentiment(newTweet.tweetText)*100).round
-		newTweet.save
+
+Friend.all.each do |handle|
+	tweets = CLIENT.user_timeline(handle.twitterHandle)
+	(10).downto(0).each do |i|
+		newTweet = Tweet.new
+		newTweet.tweetTime = tweets[i].created_at.to_i
+		lastTweetTime = Tweet.where(twitterHandle:handle.twitterHandle).maximum("tweetTime")
+		if lastTweetTime == nil or newTweet.tweetTime > lastTweetTime
+			newTweet.twitterHandle = handle.twitterHandle
+			newTweet.tweetText = tweets[i].text.chomp
+			newTweet.tweetScore = (Indico.sentiment(newTweet.tweetText)*100).round
+			newTweet.save
+		end
 	end
 end
